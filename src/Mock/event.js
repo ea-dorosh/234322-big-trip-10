e922445d8c0fb4ac0bdefc935cd9c9ec071c28da
Egor <dorosh.ea@yandex.ru>
2019-12-04 10:56:48 +0400

    start

---
+++ src/Mock/event.js
@@ -0,0 +1,12 @@
+const eventIcon = {
+  check: `check-in.png`,
+  sightseeing: `sightseeing.png`,
+  restaurant: `restaurant.png`,
+  taxi: `taxi.png`,
+  bus: `bus.png`,
+  train: `train.png`,
+  ship: `ship.png`,
+  transport: `transport.png`,
+  drive: `drive.png`,
+  flight: `flight.png`,
+};
